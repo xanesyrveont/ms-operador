965346321e20c1ee1f2e72e68038bcf234e714a6
Frank Montalvo Ochoa <fgmo.dev@fmontalvoo.com>
2024-07-05 19:41:38 -0500

    Modifico DTO del Libro
    Signed-off-by: Frank Montalvo Ochoa <fgmo.dev@fmontalvoo.com>

--- src/main/java/net/unir/grupo_12/operador/entity/dto/Libro.java
+++ src/main/java/net/unir/grupo_12/operador/entity/dto/Libro.java
@@ -1,12 +1,16 @@
 package net.unir.grupo_12.operador.entity.dto;
 
+import java.time.LocalDate;
+
 public record Libro(
         String id,
+        String isbn,
         String titulo,
         String autor,
-        String isbn,
-        Long anio,
+        String genero,
+        String portada,
         Long edicion,
-        String editorial
+        String editorial,
+        LocalDate fechaPublicacion
 ) {
 }
